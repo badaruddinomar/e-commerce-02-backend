890666b3e3fbfebcde75a78be13838af9bbaf850
badaruddinomar <badaruddinomar2403@gmail.com>
2024-07-01 12:51:05 +0600

    file folder setup

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,12 @@
 // Packages--
 import express from "express";
-import dotenv from "dotenv/config";
-
 // Instances--
 const app = express();
-
 // Middleware--
-
 // DB connection--
-
 // Routes--
-
 // Server connection--
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`server running on port:${port}`);
+    console.log(`server running on port:${port}`);
 });
