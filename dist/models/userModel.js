0360bd95a7661ae7fb89e3f6ea70b3bc3c47cf44
badaruddinomar <badaruddinomar2403@gmail.com>
2024-07-01 13:02:31 +0600

    user model created

--- dist/models/userModel.js
+++ dist/models/userModel.js
@@ -1,1 +1,50 @@
-export {};
+import mongoose from "mongoose";
+import validator from "validator";
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: [true, "Please enter ID"],
+    },
+    name: {
+        type: String,
+        required: [true, "Please enter name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter email"],
+        unique: [true, "Email already exists"],
+        validate: [validator.default.isEmail, "Please enter valid email"],
+    },
+    photo: {
+        type: String,
+        required: [true, "Please enter photo"],
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
+    gender: {
+        type: String,
+        enum: ["female", "male"],
+        required: [true, "Please enter gender"],
+    },
+    dob: {
+        type: Date,
+        required: [true, "Please enter your date of birth"],
+    },
+}, {
+    timestamps: true,
+});
+userSchema.virtual("age").get(function () {
+    const today = new Date();
+    const dob = this.dob;
+    let age = today.getFullYear() - dob.getFullYear();
+    if (today.getMonth() < dob.getMonth() ||
+        (today.getMonth() === dob.getDate() && today.getDate() < dob.getDate())) {
+        age--;
+    }
+    return age;
+});
+const User = mongoose.model("User", userSchema);
+export default User;
---
+++ src/models/userModel.ts
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+interface UserSchemaType extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  photo: string;
+  role: "admin" | "user";
+  gender: "male" | "female";
+  dob: Date;
+  age: number; // virtual attribute--
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: [true, "Please enter ID"],
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter email"],
+      unique: [true, "Email already exists"],
+      validate: [validator.default.isEmail, "Please enter valid email"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Please enter photo"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    gender: {
+      type: String,
+      enum: ["female", "male"],
+      required: [true, "Please enter gender"],
+    },
+    dob: {
+      type: Date,
+      required: [true, "Please enter your date of birth"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.virtual("age").get(function () {
+  const today = new Date();
+  const dob = this.dob;
+  let age = today.getFullYear() - dob.getFullYear();
+  if (
+    today.getMonth() < dob.getMonth() ||
+    (today.getMonth() === dob.getDate() && today.getDate() < dob.getDate())
+  ) {
+    age--;
+  }
+  return age;
+});
+
+const User = mongoose.model<UserSchemaType>("User", userSchema);
+export default User;
